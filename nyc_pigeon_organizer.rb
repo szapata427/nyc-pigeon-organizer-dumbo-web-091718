d213218f8a9441a85d9b90fb8d65f97de18e052c
Steven Zapata <szapata427@gmail.com>
2018-09-14 03:56:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,46 @@
+
+require "pry"
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+names_array = []
+  
+  data.each do |desc, info|
+  info.each do |attribute, names|
+    names.each do |name|
+    names_array << name
+    
+  end
 end
+end
+
+names_array.uniq!
+
+pigeon_hash = {}
+
+names_array.each do |name|
+  
+  pigeon_hash[name] = {}
+  
+  data.keys.each do |desc|
+    # binding.pry
+    pigeon_hash[name][desc] = []
+  end
+end
+
+names_array.each do |name|
+  data.each do |desc, info|
+   info.each do |attribute, value|
+    
+      if value.include?(name)
+  
+      pigeon_hash[name][desc] << attribute.to_s
+end
+end
+end
+end
+pigeon_hash
+  
+end
+ 
+  
+  

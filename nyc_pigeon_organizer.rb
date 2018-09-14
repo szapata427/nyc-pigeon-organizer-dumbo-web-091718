2c40f3392745be3fb90c733d186b522717fbfae4
Steven Zapata <szapata427@gmail.com>
2018-09-14 03:21:18 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,12 +1,12 @@
 
-require "pry"
+
 def nyc_pigeon_organizer(data)
   new_hash = {}
   
   data.each do |desc, info|
     var = desc
    info.each do |info, value|
-    
+    require "pry"
   if new_hash.include?(info)
     
     new_hash[info][:desc] << var

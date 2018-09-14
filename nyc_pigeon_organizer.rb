6f64a1e5c55848b8ae19e5d6bcb960b5553bbec5
Steven Zapata <szapata427@gmail.com>
2018-09-14 03:36:07 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,11 +6,13 @@ def nyc_pigeon_organizer(data)
   data.each do |desc, info|
   info.each do |info, names|
     names_array << names
-    names_array2 = names_array.flatten.uniq
-end
+    
   end
-  names_array2
 end
+names_array.flatten.uniq
 
+pigeon_hash = {}
+
+end
  
   
